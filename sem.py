b1a7c642ae6afe7fc674ea2a7351cbbe7587cd98
Shakiriya <115091523+RiyaKamath@users.noreply.github.com>
2023-11-16 23:24:58 +0530

    Created sem.py

---
+++ sem.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+from threading import Semaphore
+
+app = Flask(_name_, static_url_path='', static_folder='.')
+semaphore = Semaphore(1)  # Only one thread can access the resource at a time
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/pay', methods=['POST'])
+def pay():
+    semaphore.acquire()
+    try:
+        return 'Payment processed', 200
+    finally:
+        semaphore.release()
+
+if _name_ == '_main_':
+    app.run(port=5000)
